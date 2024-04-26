553dccd3a841cdcc906a7f01b7d9fb113e7f55d1
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-04-26 23:34:53 +0500

    feat: disable notify

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "lukas-reineke/indent-blankline", enabled = false },
-  { "nvim-neo-tree/neo-tree",         enabled = false },
+  { "nvim-neo-tree/neo-tree", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
