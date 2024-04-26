43fa60b68130f9c9a254e586002ece1ec6ea0f18
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-04-27 00:11:08 +0500

    feat: disable terminal

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "lukas-reineke/indent-blankline", enabled = false },
   { "nvim-neo-tree/neo-tree", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "akinsho/toggleterm.nvim", enabled = false },
 }
